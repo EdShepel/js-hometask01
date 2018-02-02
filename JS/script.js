ad736b11e6181b38cc1304c96b8174ffbf816e3a
EdShepel <edd.shepel@gmail.com>
2018-02-02 14:46:58 +0200

    Initial commit

---
+++ JS/script.js
@@ -0,0 +1,24 @@
+ var login = prompt('Введите логин?', '');
+
+if (login == 'Админ') {
+
+  var password = prompt('Пароль?', '');
+
+  if (password == 'Чёрный Властелин') {
+    alert( 'Добро пожаловать!' );
+  } else if (password == null) { 
+    alert( 'Вход отменён' );
+  } else {
+    alert( 'Пароль неверен' );
+  }
+
+} else if (login == null) { 
+  alert( 'Вход отменён' );
+
+} else {
+
+  alert( 'Я вас не знаю' );
+
+}
+
+
